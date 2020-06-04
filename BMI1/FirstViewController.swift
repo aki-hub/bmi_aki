5bb7be393a8545b1884b81975db582761a735765
aki-hub <akithegod710@gmail.com>
2020-06-05 02:19:57 +0900

    add bmi

--- BMI1/FirstViewController.swift
+++ BMI1/FirstViewController.swift
@@ -18,12 +18,14 @@ class FirstViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var resultButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
     
+    
     override func viewDidLoad() {
            super.viewDidLoad()
 
            secondLabel.numberOfLines = 3
            secondLabel.text = "前回の健康診断の結果はいかがでしたか？\n現在のBMI指数を把握して\n健康を維持しましょう。"
-           
+           weightTextField.text = "体重"
+                  heightTextField.text = "身長"
            //青色の設定
            let rgba = UIColor(red:55/255,green:200/255,blue: 214/255,alpha: 1.0)
            //ボタンの角の設定
@@ -68,19 +70,46 @@ class FirstViewController: UIViewController,UITextFieldDelegate {
     //Storyboardから遷移先のViewControllerを生成
            //簡易的なコードなので強制キャストしている。
            let vc = UIStoryboard(name: "SecondViewController", bundle: nil).instantiateInitialViewController()as! SecondViewController
+        
+        if weightTextField.text != nil {
+            if heightTextField.text != nil {
+        var doubleWeight:Double = atof(weightTextField.text)
+        var doubleHeight:Double = atof(heightTextField.text)/100
+        
+        var bmi:Double = doubleWeight / (doubleHeight * doubleHeight)
+        var goodWeight:Double = 22 * doubleHeight * doubleHeight
+        var hikaku:Double = doubleWeight - goodWeight
+        
+        bmi = round(bmi*10)/10
+        goodWeight = round(goodWeight*10)/10
+        hikaku = round(hikaku*10)/10
+                
+        vc.bmi = bmi
+        vc.goodWeight = goodWeight
+        vc.hikaku = hikaku
            //present()で遷移する
            present(vc,animated: true,completion: nil)
-
-           //let weight:Double = atof(weightField.text)
-           vc.weight = weightTextField.text
-           //let height:Double = atof(heightField.text)
-           vc.height = heightTextField.text
+            
+        } else {
+                secondLabel.text = "数値を入力してください"
+        }
     
-    }
+        } else {
+            secondLabel.text = "数値を入力してください"
+        }
+        }
     
     @IBAction func resetButton(_ sender: Any) {
-        weightTextField.placeholder = ""
-        heightTextField.placeholder = ""
+        weightTextField.text = ""
+        heightTextField.text = ""
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+       //viewDidLoadの処理
+        weightTextField.text = ""
+        heightTextField.text = ""
+       
     }
     
     // キーボードを閉じる（returnキーを押下時）
--- BMI1/SecondViewController.swift
+++ BMI1/SecondViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import Darwin
 
 class SecondViewController: UIViewController {
 
-    var weight:String!
-    var height:String!
     var bmi:Double!
+    var goodWeight:Double!
+    var hikaku:Double!
+
     
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var goodLabel: UILabel!
@@ -23,21 +25,45 @@ class SecondViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(weight)
-        print(height)
-        print(bmi)
 
         // パーツの設定
-        waku2Label.layer.cornerRadius = 20.0
         resetButton.layer.cornerRadius = 5.0
+        waku2Label.layer.cornerRadius = 20
+        waku2Label.layer.backgroundColor = UIColor.white.cgColor
         waku2Label.layer.borderColor = UIColor.gray.cgColor
-        waku2Label.layer.borderWidth = 0.1
         waku2Label.layer.shadowColor = UIColor.black.cgColor
         waku2Label.layer.shadowRadius = 5
         waku2Label.layer.shadowOffset = CGSize(width: 5, height: 5)
         waku2Label.layer.shadowOpacity = 0.1
+        waku2Label.layer.borderWidth = 0.1
+        
+        if 25 <= bmi && bmi < 30 {
+            resultLabel.text = String("\(bmi!) ") + "　肥満（１度)"
+        } else if 30 <= bmi && bmi < 35 {
+            resultLabel.text = String("\(bmi!) ") + "　肥満（２度)"
+        } else if 35 <= bmi && bmi < 40 {
+            resultLabel.text = String("\(bmi!) ") + "　肥満（３度）"
+        } else if 40 <= bmi {
+            resultLabel.text = String("\(bmi!)") + "　肥満（４度）"
+        } else if 18.5 <= bmi && bmi < 25 {
+            resultLabel.text = String("\(bmi!)") + "　普通体重"
+        } else if bmi < 18.5 {
+            resultLabel.text = String("\(bmi!)") + "  低体重"
+        }
+        goodLabel.text = String("\(goodWeight!)") + "　kg"
+        hikakuLabel.text = String("\(hikaku!)") + "　kg"
         
     }
     
+    @IBAction func resetButton(_ sender: Any) {
+        /*
+        let vc = UIStoryboard(name: "FirstViewController", bundle: nil).instantiateInitialViewController()as! FirstViewController
+        
+        vc.weightTextField.placeholder = ""
+        vc.heightTextField.placeholder = ""
+    */
+        //present()で遷移する
+        dismiss(animated: true, completion: nil)
+    }
     
 }
