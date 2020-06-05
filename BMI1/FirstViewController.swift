76a89438ccf2d089334254fcf128fd642520640a
hayabusabusa <hayabusabusaSY@gmail.com>
2020-06-05 17:56:43 +0900

    extension を利用して、UIのセットアップを部品ごとに分解、ViewDidLoad内を整理

--- BMI1/FirstViewController.swift
+++ BMI1/FirstViewController.swift
@@ -21,41 +21,11 @@ class FirstViewController: UIViewController,UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //文字の太さ
-        firstLabel.font = UIFont.boldSystemFont(ofSize: 17)
-        secondLabel.font = UIFont.boldSystemFont(ofSize: 15)
-    
-        secondLabel.numberOfLines = 3
-        secondLabel.text = "前回の健康診断の結果はいかがでしたか？\n現在のBMI指数を把握して\n健康を維持しましょう。"
-
-        //青色の設定
-        let rgba = UIColor(red: 55/255, green: 200/255, blue: 214/255, alpha: 1.0)
-        //ボタンの角の設定
-        resultButton.layer.cornerRadius = 5.0
-       
-        resetButton.layer.borderColor = rgba.cgColor
-        resetButton.layer.borderWidth = 0.8
-        resetButton.layer.cornerRadius = 5.0
-        resetButton.backgroundColor = UIColor.white
-       
-        waku1Label.layer.cornerRadius = 20
-        waku1Label.layer.backgroundColor = UIColor.white.cgColor
-        waku1Label.layer.borderColor = UIColor.gray.cgColor
-        waku1Label.layer.shadowColor = UIColor.black.cgColor
-        waku1Label.layer.shadowRadius = 5
-        waku1Label.layer.shadowOffset = CGSize(width: 5, height: 5)
-        waku1Label.layer.shadowOpacity = 0.1
-        waku1Label.layer.borderWidth = 0.1
-        
-        //textFieldの下線作成
-        weightTextField.placeholder = "NAME"
-        weightTextField.addBorderBottom(height: 1.0, coler: UIColor.lightGray)
-        heightTextField.addBorderBottom(height: 1.0, coler: UIColor.lightGray)
-        heightTextField.placeholder = "NAME"
-        weightTextField.delegate = self
-        heightTextField.delegate = self
-        weightTextField.placeholder = "体重"
-        heightTextField.placeholder = "身長"
+        configureLabels()
+        configureResetButton()
+        configureWaku1Label()
+        configureWeightTextField()
+        configureHeightTextField()
     }
     
     override func didReceiveMemoryWarning() {
@@ -114,6 +84,54 @@ class FirstViewController: UIViewController,UITextFieldDelegate {
         return true
     }
     
+}
+
+// MARK: - Configure
+
+extension FirstViewController {
+    
+    private func configureLabels() {
+        firstLabel.font = UIFont.boldSystemFont(ofSize: 17)
+        secondLabel.font = UIFont.boldSystemFont(ofSize: 15)
+        secondLabel.numberOfLines = 3
+        secondLabel.text = "前回の健康診断の結果はいかがでしたか？\n現在のBMI指数を把握して\n健康を維持しましょう。"
+    }
+    
+    private func configureResetButton() {
+        //ボタンの角の設定
+        resultButton.layer.cornerRadius = 5.0
+        
+        resetButton.layer.borderColor = UIColor(red: 55/255, green: 200/255, blue: 214/255, alpha: 1.0).cgColor
+        resetButton.layer.borderWidth = 0.8
+        resetButton.layer.cornerRadius = 5.0
+        resetButton.backgroundColor = UIColor.white
+    }
+    
+    private func configureWaku1Label() {
+        // NOTE: UILabel ではなく UIView にするべき
+        waku1Label.layer.cornerRadius = 20
+        waku1Label.layer.backgroundColor = UIColor.white.cgColor
+        waku1Label.layer.borderColor = UIColor.gray.cgColor
+        waku1Label.layer.shadowColor = UIColor.black.cgColor
+        waku1Label.layer.shadowRadius = 5
+        waku1Label.layer.shadowOffset = CGSize(width: 5, height: 5)
+        waku1Label.layer.shadowOpacity = 0.1
+        waku1Label.layer.borderWidth = 0.1
+    }
+    
+    private func configureWeightTextField() {
+        weightTextField.placeholder = "NAME"
+        weightTextField.addBorderBottom(height: 1.0, coler: UIColor.lightGray)
+        weightTextField.delegate = self
+        weightTextField.placeholder = "体重"
+    }
+    
+    private func configureHeightTextField() {
+        heightTextField.addBorderBottom(height: 1.0, coler: UIColor.lightGray)
+        heightTextField.placeholder = "NAME"
+        heightTextField.delegate = self
+        heightTextField.placeholder = "身長"
+    }
 }
 
 extension UITextField {
